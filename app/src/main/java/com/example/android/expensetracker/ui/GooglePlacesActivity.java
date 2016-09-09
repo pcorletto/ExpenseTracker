1127182c8eff814be22b0c951f09d5c55e7794b3
Peter Corletto <peter.corletto@gmail.com>
2016-09-08 20:27:15 -0400

    31st commit: Added a Toast to display the name and address of a place on the marker. Also inserted a CalculateDistance function that returns the distance in miles, approximate, from the current GPS location to the location of the place on the tapped marker

--- app/src/main/java/com/example/android/expensetracker/ui/GooglePlacesActivity.java
+++ app/src/main/java/com/example/android/expensetracker/ui/GooglePlacesActivity.java
@@ -24,12 +24,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
+import java.text.DecimalFormat;
+
 public class GooglePlacesActivity extends FragmentActivity implements LocationListener {
 
     private static final String GOOGLE_API_KEY = "";
     GoogleMap googleMap;
-    double latitude = 0;
-    double longitude = 0;
+    double currentLatitude = 0;
+    double currentLongitude = 0;
+    double placeLatitude = 0;
+    double placeLongitude = 0;
+    double distance = 0;
     private int PROXIMITY_RADIUS = 5000;
     String storeName;
 
@@ -70,7 +75,7 @@ public class GooglePlacesActivity extends FragmentActivity implements LocationLi
 
         Toast.makeText(GooglePlacesActivity.this, storeName, Toast.LENGTH_LONG).show();
         StringBuilder googlePlacesUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
-        googlePlacesUrl.append("location=" + latitude + "," + longitude);
+        googlePlacesUrl.append("location=" + currentLatitude + "," + currentLongitude);
         googlePlacesUrl.append("&radius=" + PROXIMITY_RADIUS);
         googlePlacesUrl.append("&name=" + storeName);
         googlePlacesUrl.append("&sensor=true");
@@ -86,9 +91,18 @@ public class GooglePlacesActivity extends FragmentActivity implements LocationLi
             @Override
             public boolean onMarkerClick(Marker marker) {
 
-                String markerTitle = marker.getTitle().toString();
 
-                Toast.makeText(GooglePlacesActivity.this, markerTitle, Toast.LENGTH_LONG).show();
+                placeLatitude = marker.getPosition().latitude;
+
+                placeLongitude = marker.getPosition().longitude;
+
+                distance = calculateDistance(currentLatitude, currentLongitude, placeLatitude,
+                        placeLongitude);
+
+                DecimalFormat df = new DecimalFormat("#.#");
+
+                Toast.makeText(GooglePlacesActivity.this, marker.getTitle().toString() +
+                        "\n" + df.format(distance) + " miles away.", Toast.LENGTH_LONG).show();
 
                 return true;
             }
@@ -124,12 +138,31 @@ public class GooglePlacesActivity extends FragmentActivity implements LocationLi
         }
     }
 
+    public double calculateDistance(double latitude1, double longitude1, double latitude2, double longitude2){
+
+        int radius=6371; // radius of the Earth in Km
+        double lat1 = latitude1;
+        double lat2 = latitude2;
+        double lon1 = longitude1;
+        double lon2 = longitude2;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLon/2) * Math.sin(dLon/2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double distInKms = radius * c;  // Distance in Kilometers
+        double distInMiles = distInKms * 0.62137119;  // Distance converted to Miles
+
+        return distInMiles;
+    }
+
 
     @Override
     public void onLocationChanged(Location location) {
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
-        LatLng latLng = new LatLng(latitude, longitude);
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
     }
